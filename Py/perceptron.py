add40506c50fecf9148855ec176f46ad0fa2bf4b
Amit Kumar Pathak <pathakamitkumar67@gmail.com>
2021-07-21 14:08:32 +0530

    Perceptron In neural networks

---
+++ Py/perceptron.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 12:36:11 2021
+
+@author: ThisPC
+"""
+"""
+perceptron:- A Perceptron is an algorithm used for supervised learning of binary classifiers.
+Binary classifiers decide whether an input, usually represented by a series of vectors, belongs to a specific class.
+
+In short, a perceptron is a single-layer neural network. They consist of four main parts including input values, weights and bias, net sum, and an activation function.
+
+Working:- The process begins by taking all the input values and multiplying them by their weights.
+Then, all of these multiplied values are added together to create the weighted sum.
+The weighted sum is then applied to the activation function, producing the perceptron's output.
+The activation function plays the integral role of ensuring the output is mapped between required values such as (0,1) or (-1,1).
+It is important to note that the weight of an input is indicative of the strength of a node.
+Similarly, an input's bias value gives the ability to shift the activation function curve up or down
+
+"""
+
+
+import numpy as np
+
+class Perceptron:
+	def __init__(self, N, alpha=0.1):
+		# initialize the weight matrix and store the learning rate
+		self.W = np.random.randn(N + 1) / np.sqrt(N)
+		self.alpha = alpha
+
+	def step(self, x):
+		# apply the step function
+		return 1 if x > 0 else 0
+
+	def fit(self, X, y, epochs=10):
+		# insert a column of 1's as the last entry in the feature
+		# matrix -- this little trick allows us to treat the bias
+		# as a trainable parameter within the weight matrix
+		X = np.c_[X, np.ones((X.shape[0]))]
+
+		# loop over the desired number of epochs
+		for epoch in np.arange(0, epochs):
+			# loop over each individual data point
+			for (x, target) in zip(X, y):
+				# take the dot product between the input features
+				# and the weight matrix, then pass this value
+				# through the step function to obtain the prediction
+				p = self.step(np.dot(x, self.W))
+
+				# only perform a weight update if our prediction
+				# does not match the target
+				if p != target:
+					# determine the error
+					error = p - target
+
+					# update the weight matrix
+					self.W += -self.alpha * error * x
+
+	def predict(self, X, addBias=True):
+		# ensure our input is a matrix
+		X = np.atleast_2d(X)
+
+		# check to see if the bias column should be added
+		if addBias:
+			# insert a column of 1's as the last entry in the feature
+			# matrix (bias)
+			X = np.c_[X, np.ones((X.shape[0]))]
+
+		# take the dot product between the input features and the
+		# weight matrix, then pass the value through the step
+		# function
+		return self.step(np.dot(X, self.W))
+# construct the OR dataset
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [1]])
+
+# define our perceptron and train it
+print("[INFO] training perceptron...")
+p = Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=20)
+# now that our perceptron is trained we can evaluate it
+print("[INFO] testing perceptron...")
+
+# now that our network is trained, loop over the data points
+for (x, target) in zip(X, y):
+	# make a prediction on the data point and display the result
+	# to our console
+	pred = p.predict(x)
+	print("[INFO] data={}, ground-truth={}, pred={}".format(
+		x, target[0], pred))
+    
+# construct the AND dataset
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [0], [0], [1]])
+
+# define our perceptron and train it
+print("[INFO] training perceptron...")
+p = Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=20)
+
+# now that our perceptron is trained we can evaluate it
+print("[INFO] testing perceptron...")
+
+# now that our network is trained, loop over the data points
+for (x, target) in zip(X, y):
+	# make a prediction on the data point and display the result
+	# to our console
+	pred = p.predict(x)
+	print("[INFO] data={}, ground-truth={}, pred={}".format(
+		x, target[0], pred))
+    
+# construct the XOR dataset
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+# define our perceptron and train it
+print("[INFO] training perceptron...")
+p = Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=20)
+
+# now that our perceptron is trained we can evaluate it
+print("[INFO] testing perceptron...")
+
+# now that our network is trained, loop over the data points
+for (x, target) in zip(X, y):
+	# make a prediction on the data point and display the result
+	# to our console
+	pred = p.predict(x)
+	print("[INFO] data={}, ground-truth={}, pred={}".format(
+		x, target[0], pred))
